16bb3e99d7dbc9ff9d8187fd5f67f8e0e3661874
Soma Zsják <sanraith@users.noreply.github.com>
2024-10-08 20:04:59 +0200

    Added led lighting effect.

--- src/sketch_oct8a/sketch_oct8a.ino
+++ src/sketch_oct8a/sketch_oct8a.ino
@@ -1,42 +1,41 @@
-/*
-  Blink
+/*****************  NEEDED TO MAKE NODEMCU WORK ***************************/
+#define FASTLED_INTERRUPT_RETRY_COUNT 0
+#define FASTLED_ESP8266_RAW_PIN_ORDER
+/******************  LIBRARY SECTION *************************************/
+#include <FastLED.h>
+/*****************  LED LAYOUT AND SETUP *********************************/
+#define NUM_LEDS 60 //+3
+/*****************  DECLARATIONS  ****************************************/
+CRGB leds[NUM_LEDS];
+/*****************  GLOBAL VARIABLES  ************************************/
+const int ledPin = 4; // marked as D2 on the board
 
-  Turns an LED on for one second, then off for one second, repeatedly.
+int idx = 1;
+/*****************  SETUP FUNCTIONS  ****************************************/
+void setup()
+{
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-*/
-
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+  FastLED.addLeds<WS2813, ledPin, GRB>(leds, NUM_LEDS);
 }
-
-// the loop function runs over and over again forever
-void loop() {
-  while (Serial.available() >  0) {
-    Serial.write(Serial.read());
+/*****************  MAIN LOOP  ****************************************/
+void loop()
+{
+  if (idx >= NUM_LEDS)
+  {
+    idx = 1;
   }
+  if (leds[idx].r > 0)
+  {
+    leds[idx] = CRGB(0, 0, 0);
+  }
+  else
+  {
+    leds[idx] = CRGB(255, 255, 0);
+  }
+  idx = idx + 2;
 
-  // digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  // delay(100);                      // wait for a second
-  // digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  // delay(1000);                      // wait for a second
+  FastLED.setBrightness(25);
+  FastLED.show();
+  delay(75);
 }
